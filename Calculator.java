39e598328b59df38831106bc2e602e570695947b
srivarshini-15 <2420080015@klh.edu.in>
2025-07-29 14:51:25 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,24 @@
+package example;
+
+public class Calculator {
+
+public class Array1 {
+    public static void main(String args[])
+    {
+    	int a[];
+    	a=new int[5];
+    	a[0]=10;
+    	a[1]=20;
+    	
+    	a[2]=30;a[3]=40;
+    	a[4]=50;
+    	System.out.println("array is"+a[3]);
+    	for(int i=0; i<5; i++)
+    	{
+    		System.out.println("array is"+a[i]);
+    	}
+  }
+
+}
+
+}
