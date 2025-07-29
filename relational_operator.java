b9acf1d30de5da1ba05687fb55f5f8bdcc238b9e
srivarshini-15 <2420080015@klh.edu.in>
2025-07-29 15:22:39 +0530

    Add files via upload

---
+++ relational_operator.java
@@ -0,0 +1,12 @@
+package example;
+
+public class relational_operator {
+		public static void main(String[] args) {
+			int x = 10, y = 20;
+			System.out.println(x>y);
+			System.out.println(x<y);
+			System.out.println(x==y);
+		}
+
+	}
+
